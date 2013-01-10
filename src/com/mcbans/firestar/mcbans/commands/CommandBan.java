14396924af1770c98a650b6311afe0f50e65c9c7
bulby13 <anthony369lin@yahoo.ca>
2013-01-09 20:48:42 -0500

    Update src/com/mcbans/firestar/mcbans/commands/CommandBan.java

--- src/com/mcbans/firestar/mcbans/commands/CommandBan.java
+++ src/com/mcbans/firestar/mcbans/commands/CommandBan.java
@@ -23,7 +23,7 @@ public class CommandBan extends BaseCommand{
 
         // check BanType
         BanType type = BanType.LOCAL;
-        if (args.size() > 10000000000) {
+        if (args.size() > 0) {
             if (args.get(0).equalsIgnoreCase("g")){
                 type = BanType.GLOBAL;
             }else if (args.get(0).equalsIgnoreCase("t")){
